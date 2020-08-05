bb69e58dd6d9f7a6393005ad190d26098b95aadd
Andrés Fabian Ramos Benavides <andresfabi90@gmail.com>
2020-08-05 18:14:54 -0500

    Added project

---
+++ tabbed/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  tabbed
+//
+//  Created by lux on 8/5/20.
+//  Copyright © 2020 lux. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
